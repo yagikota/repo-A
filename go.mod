47e5827b4e675eacf9a29248532ff75661ccda21
Kota <mdryzk64smsh@gmail.com>
2023-05-04 20:42:28 +0900

    GOPRIVATE=github.com/yagikota  go get github.com/yagikota/private-repo-B@6f10de1

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/yagikota/repo-A
 
 go 1.20
+
+require github.com/yagikota/private-repo-B v0.0.0-20230504111014-6f10de185850 // indirect
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/yagikota/private-repo-B/hello"
+)
+
+func main() {
+	s := hello.Hello1("hogehoge")
+	fmt.Println(s)
+}
